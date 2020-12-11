86ab0b87ebafc74086dcfcc38978c514e750e5a8
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-12-11 10:19:34 +0200

    need to fix cardAddTiFavorites

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import fetchCategories from './js/fetchCategories'
 import fullscreen from './images/fullscreen.png';
 import heart from './images/heart.png';
 import heartfill from './images/heartfill.png';
-import './js/cardAddToFavorites'
+// import './js/cardAddToFavorites'
 import './componentsJS/addModal';
-import './componentsJS/prodCard';
+// import './componentsJS/prodCard';  
 import './js/renderComponentsHTML';
 
