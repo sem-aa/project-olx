cda4a113f1e6d93784bfc5792046fff0cef1d2f1
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2020-12-12 14:41:06 +0200

    add fonts

--- src/js/myCalls.js
+++ src/js/myCalls.js
@@ -28,20 +28,20 @@ const optionAdd = {
 fetch(urlAdd, optionAdd).then(r => r.json()).then(console.log)
 */
 
-// const currentProd = {
-//   imageUrls: [
-//     'https://storage.googleapis.com/kidslikev2_bucket/07f526f8-4cbe-4d47-8b3a-ffd3cdc3e150.jpeg',
-//   ],
-//   _id: '5fd076b7deae5f0017e41a7d',
-//   title: 'Red Shirt',
-//   description: 'New red shirt, made from cotton',
-//   category: 'business and services',
-//   price: 255,
-//   phone: '+380000000000',
-//   isOnSale: false,
-//   userId: '5fcf371a2d55d90017ae3391',
-//   __v: 0,
-// }
+const currentProd = {
+  imageUrls: [
+    'https://storage.googleapis.com/kidslikev2_bucket/07f526f8-4cbe-4d47-8b3a-ffd3cdc3e150.jpeg',
+  ],
+  _id: '5fd076b7deae5f0017e41a7d',
+  title: 'Red Shirt',
+  description: 'New red shirt, made from cotton',
+  category: 'business and services',
+  price: 255,
+  phone: '+380000000000',
+  isOnSale: false,
+  userId: '5fcf371a2d55d90017ae3391',
+  __v: 0,
+}
 
 const url = `${BASE_URL}/call/${currentProd._id}`
 let patchToUpdate = {};
